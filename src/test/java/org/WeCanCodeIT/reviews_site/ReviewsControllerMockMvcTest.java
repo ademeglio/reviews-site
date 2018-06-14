c8c64a42a800bdd6332e5374536f696577ef4472
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-06-14 19:01:17 -0400

    test routing to view

--- src/test/java/org/WeCanCodeIT/reviews_site/ReviewsControllerMockMvcTest.java
+++ src/test/java/org/WeCanCodeIT/reviews_site/ReviewsControllerMockMvcTest.java
@@ -7,8 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 @RunWith(SpringRunner.class)
@@ -23,4 +26,8 @@ public class ReviewsControllerMockMvcTest {
 		mvc.perform(get("/show-reviews")).andExpect(status().isOk());
 	}
 	
+	@Test
+	public void shouldRouteToAllCoursesView() throws Exception {
+		mvc.perform(get("/show-reviews")).andExpect(view().name(is("reviews")));
+	}
 }
