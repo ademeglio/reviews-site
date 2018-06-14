632f90da22a67e7dd848ff7d751d96aec15ccca3
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-06-14 19:35:50 -0400

    ReviewController() build mapping; add model

--- src/main/java/org/WeCanCodeIT/reviews_site/ReviewController.java
+++ src/main/java/org/WeCanCodeIT/reviews_site/ReviewController.java
@@ -1,14 +1,19 @@
 package org.WeCanCodeIT.reviews_site;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReviewController {
+	@Resource
+	ReviewRepository reviewRepo;
 	
 	@RequestMapping("/show-reviews")
-	public String findAllCourses() { 
-		
+	public String findAllCourses(Model model) { 
+		model.addAttribute("reviews", reviewRepo.findAll());
 		return "reviews";
 	}
 } // End ReviewController()
