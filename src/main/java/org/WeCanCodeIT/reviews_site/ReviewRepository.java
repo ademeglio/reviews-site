4f66374babe26f8542c86ab07cdd7b437e0addeb
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-06-14 20:20:38 -0400

    start database build for content

--- src/main/java/org/WeCanCodeIT/reviews_site/ReviewRepository.java
+++ src/main/java/org/WeCanCodeIT/reviews_site/ReviewRepository.java
@@ -10,8 +10,32 @@ import org.springframework.stereotype.Repository;
 public class ReviewRepository {
 
 	private Map<Long, Review> reviewList = new HashMap<>();
-
-	// Constructor
+	
+	// Build Database
+	public ReviewRepository() {
+		Review review1 = new Review(1L, "review title",
+										"url", 
+										"category", 
+										"content");
+		
+		Review review2 = new Review(2L, "review title",
+										"url", 
+										"category", 
+										"content");
+		
+		Review review3 = new Review(3L, "review title",
+										"url", 
+										"category", 
+										"content");
+		
+		// add reviews to repository
+		reviewList.put(review1.getReviewId(), review1);
+		reviewList.put(review2.getReviewId(), review2);
+		reviewList.put(review3.getReviewId(), review3);
+	}
+	
+	
+	// Constructor for testing
 	public ReviewRepository(Review...reviews ) {
 		for (Review review : reviews) {
 			reviewList.put(review.getReviewId(), review);
